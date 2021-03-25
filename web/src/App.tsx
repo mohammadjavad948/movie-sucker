e4e4d18ba1f21a6994eccaf73543012bc71c3e4e
mohammad javad <mjkmo123@gmail.com>
2021-03-25 18:34:04 +0430

    add basic navbar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/navbar";
 
 function App() {
   return (
-   <div>
-     start
-   </div>
+    <div>
+        <NavBar />
+    </div>
   );
 }
 
--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -1,3 +1,14 @@
-export default function NavBar (){
-return ()
+import React from "react";
+import {AppBar, Button, IconButton, InputBase, Toolbar, Typography} from "@material-ui/core";
+
+export default function NavBar(){
+    return (
+        <AppBar position="static" color={"primary"}>
+            <Toolbar>
+                <Typography variant="h6">
+                    Movie Sucker
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    )
 }
