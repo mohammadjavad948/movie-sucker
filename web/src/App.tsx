68b856f55df3524cf87fad1956c8e35f7e7926c5
mohammad javad <mjkmo123@gmail.com>
2021-03-25 18:44:47 +0430

    add main component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from "./components/navbar";
+import Main from "./Main";
 
 function App() {
   return (
     <div>
         <NavBar />
+        <Main />
     </div>
   );
 }
