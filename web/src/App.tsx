81dd966136ea0c058da940242114d57fc123235b
mohammad javad <mjkmo123@gmail.com>
2021-03-25 18:51:58 +0430

    add dark theme

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 import './App.css';
 import NavBar from "./components/navbar";
 import Main from "./Main";
+import {createMuiTheme, CssBaseline} from "@material-ui/core";
+import { ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        type: "dark"
+    }
+});
 
 function App() {
   return (
-    <div>
-        <NavBar />
-        <Main />
-    </div>
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div>
+              <NavBar />
+              <Main />
+          </div>
+      </ThemeProvider>
   );
 }
 
