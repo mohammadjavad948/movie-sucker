b2932bdfb910c9ec5194d5e92b0c99a920f60424
mohammad javad <mjkmo123@gmail.com>
2021-03-27 15:10:19 +0430

    add styles

--- web/src/Main.tsx
+++ web/src/Main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './main.module.css';
 
 export default function Main(){
     return (
-        <div>
+        <div className={style.container}>
             main
         </div>
     )
