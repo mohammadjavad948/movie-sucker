9382e6c530c09836a6c90a4e460bf9b56abe8920
mohammad javad <mjkmo123@gmail.com>
2021-03-28 09:42:13 +0430

    finished theme store

---
+++ web/src/stores/ThemeStore.ts
@@ -0,0 +1,24 @@
+import create, {State} from "zustand";
+
+
+interface ThemeStoreI extends State{
+    theme: string
+    dark: () => void
+    light: () => void
+}
+
+export const UseThemeStore = create<ThemeStoreI>((set) => {
+    return {
+        theme: localStorage.getItem('theme') || 'dark',
+        dark: () => {
+            localStorage.setItem('theme', 'dark')
+
+            set({theme: 'dark'})
+        },
+        light: () => {
+            localStorage.setItem('theme', 'light');
+
+            set({theme: 'light'})
+        }
+    }
+})
