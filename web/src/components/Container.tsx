3d9f110a411d2ea0ec318fb28332fe01999afd21
mohammad javad <mjkmo123@gmail.com>
2021-03-29 16:00:48 +0430

    move things to other files

--- web/src/components/Container.tsx
+++ web/src/components/Container.tsx
@@ -1,16 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import style from './Container.module.css';
 import MovieCard from "./movie";
-import axios from 'axios';
-import {API_KEY, ENDPOINT} from "../env";
 import Loading from "./Loading";
+import {discover} from "../api/api";
 
 export default function Container(){
 
     const [movie, setMovie] = useState([]);
 
     useEffect(() => {
-        axios.get(`${ENDPOINT}/discover/movie?api_key=${API_KEY}`)
+        discover()
             .then(res => {
                 setMovie(res.data.results)
             })
