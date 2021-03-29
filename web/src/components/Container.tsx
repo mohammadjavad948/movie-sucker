feb7e9583f21566dcca390d47431366d55bb2b52
mohammad javad <mjkmo123@gmail.com>
2021-03-29 15:58:43 +0430

    move loading component

--- web/src/components/Container.tsx
+++ web/src/components/Container.tsx
@@ -3,18 +3,10 @@ import style from './Container.module.css';
 import MovieCard from "./movie";
 import axios from 'axios';
 import {API_KEY, ENDPOINT} from "../env";
-import Lottie from 'react-lottie';
-import animationData from '../lotties/loading.json';
+import Loading from "./Loading";
 
 export default function Container(){
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: animationData,
-        rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
-        }
-    };
+
     const [movie, setMovie] = useState([]);
 
     useEffect(() => {
@@ -26,11 +18,7 @@ export default function Container(){
 
     return (
         <div className={style.container}>
-            {movie.length === 0 ?
-                <Lottie
-                options={defaultOptions}
-                height={250}
-                width={250}/> : ''
+            {movie.length === 0 ? <Loading height={250} width={250}/> : ''
             }
             {movie.map((el, index) => <MovieCard data={el} key={index}/>)}
         </div>
