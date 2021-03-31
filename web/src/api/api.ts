83504bd8f3e16d9b14e5585a69beb154002c3810
mohammad javad <mjkmo123@gmail.com>
2021-03-31 09:44:59 +0430

    add genres api

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -2,5 +2,9 @@ import axios from "axios";
 import {API_KEY, ENDPOINT} from "../env";
 
 export function discover(page: number){
-    return  axios.get(`${ENDPOINT}/discover/movie?api_key=${API_KEY}&page=${page}`)
+    return axios.get(`${ENDPOINT}/discover/movie?api_key=${API_KEY}&page=${page}`)
+}
+
+export function genres(){
+    return axios.get(`${ENDPOINT}/genre/movie/list?api_key=${API_KEY}`)
 }
