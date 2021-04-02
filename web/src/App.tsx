9ab13fe107043091c840e7dca368066145a077f9
mohammad javad <mjkmo123@gmail.com>
2021-04-02 22:24:21 +0430

    change theme pallete

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -11,14 +11,36 @@ function App() {
 
     const dark = createMuiTheme({
         palette: {
-            type: "dark"
-        }
+            type: 'dark',
+            background: {
+                default: '#0f0e17'
+            },
+            secondary: {
+                main: '#ff8906',
+                contrastText: '#fffffe'
+            },
+            primary: {
+                main: '#e53170',
+                contrastText: '#fffffe'
+            },
+        },
     });
 
 
     const light = createMuiTheme({
         palette: {
-            type: "light"
+            type: 'light',
+            background: {
+                default: '#fffffe'
+            },
+            secondary: {
+                main: '#ffd803',
+                contrastText: '#2d334a'
+            },
+            primary: {
+                main: '#bae8e8',
+                contrastText: '#2d334a'
+            }
         }
     });
 
