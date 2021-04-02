f18da34d9f94fcfbadab5acf911a8a2faf2dcdd3
mohammad javad <mjkmo123@gmail.com>
2021-04-02 14:59:00 +0430

    add functionality to search

--- web/src/Search.tsx
+++ web/src/Search.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {FC, useState} from 'react';
 import {Button, TextField} from "@material-ui/core";
 
 export default function Search(){
+
+    function search(query: string){
+
+    }
+
     return (
         <div>
-            <SearchBox />
+            <SearchBox searchClick={search}/>
         </div>
     )
 }
 
-function SearchBox(){
+interface SearchBoxI{
+    searchClick: (query: string) => void
+}
+
+const SearchBox: FC<SearchBoxI> = (props) => {
+
+    const [query, setQuery] = useState('');
+
+    function keyBoard(event: React.KeyboardEvent<HTMLDivElement>){
+        if (event.keyCode !== 13) return null;
+
+        props.searchClick(query);
+    }
 
     return (
         <div style={{
@@ -18,8 +35,20 @@ function SearchBox(){
             alignItems: 'center',
             justifyContent: 'center'
         }}>
-            <TextField id="outlined-basic" label="Search" variant="outlined" />
-            <Button variant={"contained"} color={"secondary"}>Go</Button>
+            <TextField
+                id="outlined-basic"
+                label="Search"
+                variant="outlined"
+                onKeyUp={keyBoard}
+                onChange={e => setQuery(e.target.value)}
+            />
+            <Button
+                variant={"contained"}
+                color={"secondary"}
+                onClick={() => props.searchClick(query)}
+            >
+                Go
+            </Button>
         </div>
     )
 }
