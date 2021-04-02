8c181573262136ecd82b083d6e1a659ede4e2f1f
mohammad javad <mjkmo123@gmail.com>
2021-04-02 23:30:15 +0430

    add main.js

---
+++ electron/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: __dirname + '/logo192.png',
+        title: 'movie sucker'
+    });
+
+    win.removeMenu();
+
+    win.loadURL('https://movie-sucker.vercel.app')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
