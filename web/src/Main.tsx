b7deb737f4800b61b54aa624e3ef8bbac6d776a3
javad <mjkmo123@gmail.com>
2021-04-04 19:41:57 +0430

    add component to router

--- web/src/Main.tsx
+++ web/src/Main.tsx
@@ -4,12 +4,16 @@ import TopBar from "./components/TopBar";
 import Container from "./components/Container";
 import {Switch, Route} from "react-router-dom";
 import Search from "./Search";
+import Detail from './Detail';
 
 export default function Main(){
     return (
             <div className={style.container}>
                 <TopBar />
                 <Switch>
+                    <Route path="/movie/:id">
+                        <Detail />
+                    </Route>
                     <Route path="/search">
                         <Search />
                     </Route>
