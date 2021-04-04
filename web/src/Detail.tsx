778fd2bfbed93f67c0adb88073011fae456476b6
javad <mjkmo123@gmail.com>
2021-04-04 19:45:50 +0430

    if user click on card it will send user to detail router

--- web/src/Detail.tsx
+++ web/src/Detail.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 
 export default function Detail(){
 
+    // @ts-ignore
+    const {id} = useParams();
+
     return (
         <div>
-            
+            {id}
         </div>
     )
 }
