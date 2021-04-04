0472f71d1fbde418c4d42b537e3d53d9a34a822f
javad <mjkmo123@gmail.com>
2021-04-04 19:57:11 +0430

    add loading

--- web/src/Detail.tsx
+++ web/src/Detail.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { details } from './api/api';
+import Loading from './components/Loading';
 
 
 export default function Detail(){
 
     // @ts-ignore
     const {id} = useParams();
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState();
+
+    useEffect(() => {
+        details(id)
+        .then(res => {
+            setLoading(false);
+            setMovie(res.data);
+        })
+        .catch(console.log);
+    }, [])
 
     return (
         <div>
-            {id}
+            {loading ? <Loading height={250} width={250}/> : ''}
         </div>
     )
 }
