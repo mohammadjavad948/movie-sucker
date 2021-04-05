8c4d3875d4b6c8b592bcfedfef1d3aca32ab2fc4
mohammad javad <mjkmo123@gmail.com>
2021-04-05 11:51:47 +0430

    add container style

--- web/src/components/MovieDetail.tsx
+++ web/src/components/MovieDetail.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import style from './MovieDetail.module.css';
 
 // @ts-ignore
 export default function MovieDetail({movie}){
 
     return (
-        <div>
+        <div className={style.container}>
 
         </div>
     )
