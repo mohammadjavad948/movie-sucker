c46dc89799085bc3ea6e0864fa34f158c7d14a18
mohammad javad <mjkmo123@gmail.com>
2021-04-05 12:01:09 +0430

    add image

--- web/src/components/MovieDetail.tsx
+++ web/src/components/MovieDetail.tsx
@@ -6,7 +6,33 @@ export default function MovieDetail({movie}){
 
     return (
         <div className={style.container}>
-
+            <FirstRow movie={movie}/>
         </div>
+    )
+}
+
+// @ts-ignore
+function FirstRow({movie}){
+
+    return (
+        <div className={style.firstRow}>
+            <Poster url={movie.poster_path}/>
+        </div>
+    )
+}
+
+// @ts-ignore
+function Poster({url}){
+
+    return (
+        <img
+            src={
+                url ? `https://image.tmdb.org/t/p/w185${url}` :
+                    'https://www.urbanbrush.net/en/wp-content/uploads/edd/2018/03/web-20180320131623142828.png'
+            }
+            alt={url}
+            height="278"
+            width="185"
+        />
     )
 }
